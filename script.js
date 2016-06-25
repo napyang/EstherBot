6ea3d24887078c8accc3f67f8f2b85f0850da36d
napyang <napyang@hotmail.com>
2016-06-26 00:40:25 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('Hi I am Naps chatting robot. 嗨，我是楊毅凡的聊天機器人。%[Hello](postback:)%[你好](postback:)')
+            return bot.say('Hi I am Naps chatting robot. 嗨，我是楊毅凡的聊天機器人。 %[Hello](postback:) %[你好](postback:)')
                 .then(() => 'speak');
         }
     },
@@ -44,7 +44,7 @@ module.exports = new Script({
                 }
 
                 if (!_.has(scriptRules, upperText)) {
-                    return bot.say(`Hi I am Naps chatting robot. 嗨，我是楊毅凡的聊天機器人。%[Hello](postback:)%[你好](postback:)`).then(() => 'speak');
+                    return bot.say(`![nap](https://raw.githubusercontent.com/napyang/EstherBot/master/img/Nap.jpg)\nHi I am Naps chatting robot. 嗨，我是楊毅凡的聊天機器人。 %[Hello](postback:) %[你好](postback:)`).then(() => 'speak');
                 }
 
                 var response = scriptRules[upperText];
